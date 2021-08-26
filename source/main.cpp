87a9f9bdbc2c59ac51bd9d87f3eda089213e900a
intns <84647527+intns@users.noreply.github.com>
2021-08-26 04:15:16 +0100

    Add options menu

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 
 #include "icon_jpg.h"
-#include "menu_change_ogg.h"
 #include "terminus_ttf.h"
 
 #include "globals.h"
@@ -19,9 +18,61 @@ enum class ProgramState : u8 {
     FadeOutText,
 
     Menu,
+    Options,
 
     MainGame
 };
+
+enum class ObjectSpawnMode : u8 {
+    Cube = 1 << 0,
+    Torus = 1 << 1,
+    Sphere = 1 << 2,
+
+    All = Cube | Torus | Sphere,
+};
+
+struct Settings {
+    // VECTOR 2 INSTANCES WORK AS: [CURRENT VALUE, MAX VALUE]
+    Vector2<u32> m_sceneObjCount = { 5, 10 };
+
+    ObjectSpawnMode m_spawnMode = ObjectSpawnMode::All;
+} gSettings;
+
+static void IncrementSpawnMode(const bool fwd)
+{
+    ObjectSpawnMode& mode = gSettings.m_spawnMode;
+    if (fwd) {
+        switch (mode) {
+        case ObjectSpawnMode::Cube:
+            mode = ObjectSpawnMode::Torus;
+            break;
+        case ObjectSpawnMode::Torus:
+            mode = ObjectSpawnMode::Sphere;
+            break;
+        case ObjectSpawnMode::Sphere:
+            mode = ObjectSpawnMode::All;
+            break;
+        case ObjectSpawnMode::All:
+            mode = ObjectSpawnMode::Cube;
+            break;
+        }
+    } else {
+        switch (mode) {
+        case ObjectSpawnMode::Cube:
+            mode = ObjectSpawnMode::All;
+            break;
+        case ObjectSpawnMode::Torus:
+            mode = ObjectSpawnMode::Cube;
+            break;
+        case ObjectSpawnMode::Sphere:
+            mode = ObjectSpawnMode::Torus;
+            break;
+        case ObjectSpawnMode::All:
+            mode = ObjectSpawnMode::Sphere;
+            break;
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -39,12 +90,26 @@ int main(int argc, char** argv)
     ProgramState state = ProgramState::FadeInText;
     float fadeTimer = 0;
 
-    Menu menu;
-    std::vector<MenuItem>& items = menu.getItems();
-    items.push_back({ 0, { 64, 241 }, "START", 46, cl::white, cl::red });
-    items.push_back({ 1, { 64, 301 }, "OPTIONS", 46, cl::white, cl::red });
-    items.push_back({ 2, { 64, 361 }, "EXIT", 46, cl::white, cl::red });
-    menu.reset(0);
+    Menu mainMenu;
+    std::vector<MenuItem>& mainMenuItems = mainMenu.getItems();
+    mainMenuItems.push_back({ 0, { 64, 241 }, "START", 46, cl::white, cl::red });
+    mainMenuItems.push_back({ 1, { 64, 301 }, "OPTIONS", 46, cl::white, cl::red });
+    mainMenuItems.push_back({ 2, { 64, 361 }, "EXIT", 46, cl::white, cl::red });
+    mainMenu.reset(0);
+
+    Menu gameMenu;
+    std::vector<MenuItem>& gameMenuItems = gameMenu.getItems();
+    gameMenuItems.push_back({ 0, { 64, 64 }, "RANDOMISE SCENE", 26, cl::white, cl::red });
+    gameMenuItems.push_back({ 1, { 64, 64 + 24 }, "RANDOMISE SIZE", 26, cl::white, cl::red });
+    gameMenuItems.push_back({ 2, { 64, 64 + 24 + 24 }, "RANDOMISE COLOURS", 26, cl::white, cl::red });
+    gameMenuItems.push_back({ 3, { 64, 64 + 24 + 24 + 24 }, "RANDOMISE LIGHTS", 26, cl::white, cl::red });
+    gameMenu.reset(0);
+
+    Menu optionsMenu;
+    std::vector<MenuItem>& optionsMenuItems = optionsMenu.getItems();
+    optionsMenuItems.push_back({ 0, { 64, 64 }, "REPLACE_WITH_CODE", 46, cl::white, cl::red });
+    optionsMenuItems.push_back({ 1, { 64, 64 + 48 }, "REPLACE_WITH_CODE", 46, cl::white, cl::red });
+    optionsMenu.reset(0);
 
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
     while (!gExit) {
@@ -124,19 +189,19 @@ int main(int argc, char** argv)
             const u32 xpos = (rmode->fbWidth / 2) - ((icon->w * scale) / 2);
             GRRLIB_DrawImg(xpos, 64, icon, 0, scale, scale, cl::white);
 
-            for (auto& item : menu.getItems()) {
+            for (MenuItem& item : mainMenuItems) {
                 item.m_position.m_x = xpos;
                 item.render(gFont);
             }
 
             if (btns_down & WPAD_BUTTON_UP) {
-                menu.moveSelected(MenuDirection::Up);
+                mainMenu.moveSelected(MenuDirection::Up);
             } else if (btns_down & WPAD_BUTTON_DOWN) {
-                menu.moveSelected(MenuDirection::Down);
+                mainMenu.moveSelected(MenuDirection::Down);
             }
 
             if (btns_down & WPAD_BUTTON_A) {
-                MenuItem& selected = menu.getSelected();
+                MenuItem& selected = mainMenu.getSelected();
 
                 // Start
                 if (selected.m_index == 0) {
@@ -144,6 +209,7 @@ int main(int argc, char** argv)
                 }
                 // Options
                 else if (selected.m_index == 1) {
+                    state = ProgramState::Options;
                 }
                 // Exit
                 else if (selected.m_index == 2) {
@@ -153,10 +219,109 @@ int main(int argc, char** argv)
 
             break;
         }
-        case ProgramState::MainGame: {
-            if (btns_down & WPAD_BUTTON_HOME) {
+        case ProgramState::Options: {
+            for (MenuItem& item : optionsMenuItems) {
+                if (item.m_index == 0) {
+                    char text[0x30];
+                    sprintf(text, "Object count: [%d / %d]", gSettings.m_sceneObjCount.m_x, gSettings.m_sceneObjCount.m_y);
+                    item.m_text = text;
+                } else if (item.m_index == 1) {
+                    char type[0x10];
+                    switch (gSettings.m_spawnMode) {
+                    case ObjectSpawnMode::All:
+                        sprintf(type, "All");
+                        break;
+                    case ObjectSpawnMode::Cube:
+                        sprintf(type, "Cube");
+                        break;
+                    case ObjectSpawnMode::Torus:
+                        sprintf(type, "Torus");
+                        break;
+                    case ObjectSpawnMode::Sphere:
+                        sprintf(type, "Sphere");
+                        break;
+                    }
+
+                    char text[0x30];
+                    sprintf(text, "Spawn type: [%s]", type);
+                    item.m_text = text;
+                }
+
+                item.render(gFont);
+            }
+
+            if (btns_down & WPAD_BUTTON_UP) {
+                optionsMenu.moveSelected(MenuDirection::Up);
+            } else if (btns_down & WPAD_BUTTON_DOWN) {
+                optionsMenu.moveSelected(MenuDirection::Down);
+            }
+
+            if (btns_down & WPAD_BUTTON_B) {
                 state = ProgramState::Menu;
-                menu.reset(0);
+            }
+
+            if (btns_down & WPAD_BUTTON_RIGHT) {
+                MenuItem& item = optionsMenu.getSelected();
+                // Scene object count+
+                if (item.m_index == 0) {
+                    gSettings.m_sceneObjCount.m_x++;
+                    if (gSettings.m_sceneObjCount.m_x > gSettings.m_sceneObjCount.m_y) {
+                        gSettings.m_sceneObjCount.m_x -= gSettings.m_sceneObjCount.m_y;
+                    }
+                } else if (item.m_index == 1) {
+                    IncrementSpawnMode(true);
+                }
+            } else if (btns_down & WPAD_BUTTON_LEFT) {
+                MenuItem& item = optionsMenu.getSelected();
+                // Scene object count-
+                if (item.m_index == 0) {
+                    gSettings.m_sceneObjCount.m_x--;
+                    if (gSettings.m_sceneObjCount.m_x == 0) {
+                        gSettings.m_sceneObjCount.m_x += gSettings.m_sceneObjCount.m_y;
+                    }
+                } else if (item.m_index == 1) {
+                    IncrementSpawnMode(false);
+                }
+            }
+
+            break;
+        }
+
+        case ProgramState::MainGame: {
+            gMainCamera->applyCamera();
+            GRRLIB_ObjectView(0, 0, -5, 0, 0, 0, 1, 1, 1);
+            GRRLIB_DrawCube(1, true, cl::white);
+
+            GRRLIB_2dMode();
+
+            for (MenuItem& item : gameMenuItems) {
+                item.render(gFont);
+            }
+
+            if (btns_down & WPAD_BUTTON_B) {
+                state = ProgramState::Menu;
+                mainMenu.reset(0);
+            }
+
+            if (btns_down & WPAD_BUTTON_UP) {
+                gameMenu.moveSelected(MenuDirection::Up);
+            } else if (btns_down & WPAD_BUTTON_DOWN) {
+                gameMenu.moveSelected(MenuDirection::Down);
+            } else if (btns_down & WPAD_BUTTON_A) {
+                MenuItem& item = gameMenu.getSelected();
+
+                // SCENE
+                if (item.m_index == 0) {
+                }
+                // SIZE
+                if (item.m_index == 1) {
+                }
+                // COLOURS
+                if (item.m_index == 2) {
+                }
+                // LIGHTS
+                if (item.m_index == 3) {
+                }
             }
 
             break;
